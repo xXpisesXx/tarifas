236a873b758336cfda5489c77b5d6adfa86ab3f2
xXpisesXx <pisesrock0.1@gmail.com>
2025-04-14 22:41:20 -0400

    Add files via upload

---
+++ listaLugares.js
@@ -0,0 +1,14 @@
+const locaciones = [
+    { nombre: "plaza bolivar", precio: '10-12' },
+    { nombre: "vinto centro", precio: '15' },
+    { nombre: "hospitar benigno sanchez", precio: '12-15' },
+    { nombre: "villa moderna", precio: '12-15' },
+    { nombre: "antofagasta norte", precio: '15-20' },
+    { nombre: "antofagasta sud", precio: '10-15' },
+    { nombre: "piscina san isidro", precio: '15-20' },
+    { nombre: "el paso", precio: '35-40' },
+    { nombre: "tiquipaya", precio: '35-40' },
+    { nombre: "kilometro 5", precio: '30' },
+    { nombre: "kilometro 10", precio: '15-20' },
+    { nombre: "puente beigin", precio: '35' }
+];
